0509824856a01fc3475f8d91e16a50761be878b8
Vytautas Valutis <dnc908@gmail.com>
2023-04-15 14:57:34 +0300

    u5 seedend DB

---
+++ bankas5/app/Http/Controllers/ClientController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use App\Http\Requests\StoreClientRequest;
+use App\Http\Requests\UpdateClientRequest;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreClientRequest $request)
+    {
+        //
+    }
+
+    public function show(Client $client)
+    {
+        //
+    }
+
+    public function edit(Client $client)
+    {
+        //
+    }
+
+    public function update(UpdateClientRequest $request, Client $client)
+    {
+        //
+    }
+
+    public function destroy(Client $client)
+    {
+        //
+    }
+}
---
+++ bankas5/database/migrations/2023_04_15_105733_create_accounts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('iban', 20);
+            $table->decimal('value', 10, 2);
+            $table->unsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
