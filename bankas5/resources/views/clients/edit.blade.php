716019f9e98c69d37f8bcd23802db1fbb722352d
Vytautas Valutis <dnc908@gmail.com>
2023-04-16 23:19:45 +0300

    U5 0416 Fin

--- bankas5/resources/views/clients/edit.blade.php
+++ bankas5/resources/views/clients/edit.blade.php
@@ -8,13 +8,12 @@
                 <div class="card-header">
                     <h1>Edit client
                         <span class="edit-header-span">PID {{$client->pid}}
-                            <button type="submit" class="btn btn-primary ms-4" form="client-edit-form">Submit</button>
                             <a href="" class="btn btn-primary ms-4">Add new account</a>
                         </span>
                     </h1>
                 </div>
                 <div class="card-body">
-                    <form action="{{route('clients-update', $client)}}" method="post" id="client-edit-form">
+                    <form action="{{route('clients-update', $client)}}" method="post">
                         <div class="mb-3">
                             <label class="form-label fs-4">Client name</label>
                             <input type="text" class="form-control" name="name" value="{{old('name', $client->name)}}">
@@ -25,32 +24,33 @@
                             <input type="text" class="form-control" name="surname" value="{{old('surname', $client->surname)}}">
                             <div class="form-text">Client surname: min 3 characters</div>
                         </div>
-                        <div class="mb-3">
-                            @forelse($accounts as $acc)
-                            <div class="container">
-                                <div class="row justify-content-left">
-                                    <div class="col-3 m-1">
-                                        <form action="" method="post">
-                                            <button type="submit" class="btn btn-outline-danger">Remove account</button>
-                                            @csrf
-                                            @method('delete')
-                                        </form>
-                                    </div>
-                                    <div class="col-3 m-2">
-                                        {{$acc->iban}}
-                                    </div>
-                                    <div class="col-4 m-2">
-                                        &#x20AC; {{number_format($acc->value, 2, '.', ' ')}}
-                                    </div>
-                                </div>
-                                </div>
-                                @empty
-                                <div>No accounts</div>
-                                @endforelse
-                            </div>
-                            @csrf
-                            @method('put')
+                            <button type="submit" class="btn btn-primary ms-4">Submit</button>
+                    @csrf
+                    @method('put')
                     </form>
+                    <div class="mb-3">
+                        @forelse($accounts as $acc)
+                        <div class="container">
+                            <div class="row justify-content-left">
+                                <div class="col-3 m-1">
+                                    <form action="" method="post">
+                                        <button type="submit" class="btn btn-outline-danger">Remove account</button>
+                                        @csrf
+                                        @method('delete')
+                                    </form>
+                                </div>
+                                <div class="col-3 m-2">
+                                    {{$acc->iban}}
+                                </div>
+                                <div class="col-4 m-2">
+                                    &#x20AC; {{number_format($acc->value, 2, '.', ' ')}}
+                                </div>
+                            </div>
+                        </div>
+                        @empty
+                        <div>No accounts</div>
+                        @endforelse
+                    </div>
                 </div>
             </div>
         </div>
